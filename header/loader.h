8e06830d5105fb21b153644eaabc9eef30ba5f81
Pranav-Bhaskar <pranavbhaskar23@gmail.com>
2019-03-08 18:22:55 +0530

    Bug Fixing
    This commit fixes the bug which could cause the program to misbehave if
    the file you were trying to load from didnot exist.

--- header/loader.h
+++ header/loader.h
@@ -18,7 +18,11 @@ Loader::Loader(std::string l){
 		file.open(fileName.c_str());
 		short header[9];
 		pix = new unsigned char [1200 * 694 * 3];
+		short expected[] = {0, 2, 0, 0, 0, 0, (short) 1200, (short) 694, 24};
 		file.read((char*)&header, sizeof(header));
+		for(int i=0;i<9;++i)
+			if(header[i] != expected[i])
+				throw "File Incorrect.";
 		file.read((char*)pix, 2498400);
 		file.close();
 		Mode* temp = new Render(pix);
